9dc669c8c825439e3ae2825ef48d122cf25e8a55
as <anon-sam@github.com>
2019-10-26 16:33:05 +0530

    modified CVEParser - adding elements to list of cves

--- src/main/java/owl/cs/netvul/btp/CVEParser.java
+++ src/main/java/owl/cs/netvul/btp/CVEParser.java
@@ -3,6 +3,7 @@ package owl.cs.netvul.btp;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -55,7 +56,10 @@ public class CVEParser {
 		File f = new File(nv.getFile());
 		//ir=IRI.create(f);
 		k=0;
-		A=List.of(2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019);
+		A=new ArrayList<>();
+		for(int i=2002;i<=2019;i++)
+			A.add(i);
+		//A=List.of(2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019);
 		man =  OWLManager.createOWLOntologyManager();
 		df= man.getOWLDataFactory();
 		try {
