1c35a317ae513fe74ee223525f8e1b39476d2291
as <anon-sam@github.com>
2020-05-21 02:20:19 +0530

    add only 2019 vulnerabilities to ontology (subset)

--- src/main/java/owl/cs/netvul/btp/CVEParser.java
+++ src/main/java/owl/cs/netvul/btp/CVEParser.java
@@ -63,11 +63,12 @@ public class CVEParser {
 		//System.out.println(nv);
 		File f = new File(nv.getFile());
 		//ir=IRI.create(f);
-		k=0;
+		
 		A=new ArrayList<>();
 		for(int i=2002;i<=2019;i++)
 			A.add(i);
 		//A=List.of(2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019);
+		k=A.size()-1;
 		man =  OWLManager.createOWLOntologyManager();
 		df= man.getOWLDataFactory();
 		try {
